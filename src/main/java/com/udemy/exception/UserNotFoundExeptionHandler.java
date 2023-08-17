e2e3d9685ecc2c22fc705c6bcdb1c71fd932d639
MarwanNahas <marwanelnahas1999@gmail.com>
2023-08-17 12:08:23 +0300

    Until Error Handling 2

--- src/main/java/com/udemy/exception/UserNotFoundExeptionHandler.java
+++ src/main/java/com/udemy/exception/UserNotFoundExeptionHandler.java
@@ -6,7 +6,7 @@ import io.micronaut.http.HttpResponse;
 import io.micronaut.http.server.exceptions.ExceptionHandler;
 import jakarta.inject.Singleton;
 
-//it will implement exeptionhandler from micronaut.http.server.exepetions not any other library the ExeptionHandler takes 2 arguments first the is T which is : Table Object so in our case its UserNotFoundExeption and the second is R which is : Return type of your handling (if this was some other web application we could have returned a rendered web page for this error )
+//it will implement exeptionhandler from micronaut.http.server.exepetions not any other library the ExeptionHandler takes 2 arguments first the is T which is : The Throwable so in our case its UserNotFoundExeption and the second is R which is :The Return Type of your handling (if this was some other web application we could have returned a rendered web page for this error )
 @Singleton
 //make this handler available only if UserNotFoundExeption,HttpResponse are ready (Make sure this bean UserNotFoundExeptionHandler is not hotwired unless thos 2 beans (classes) are available )
 @Requires(classes = {UserNotFoundExeption.class,ExceptionHandler.class})
