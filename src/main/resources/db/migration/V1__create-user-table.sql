1d4bbf1b438a389276e8d82c8a8f3902747b2d42
MarwanNahas <marwanelnahas1999@gmail.com>
2023-08-18 14:27:31 +0300

    Until Flyway

--- src/main/resources/db/migration/V1__create-user-table.sql
+++ src/main/resources/db/migration/V1__create-user-table.sql
@@ -1,7 +1,7 @@
-CREATE TABLE 'users' (
-    'id' int(11) NOT NULL AUTO_INCREMENT,
-    'name' varchar(255) DEFAULT NULL,
-    'email' varchar(255) DEFAULT NULL,
-    'mobile_number' varchar(255) DEFAULT NULL,
-    PRIMARY KEY ("id")
+-- lazem esm el class yebtedy b V w rakam el version ya3ny V1 w ba3deeha lazem 2 underscore __ ba3d keda 2ekteb ay 7aga
+create table users(
+    id int(11) auto_increment primary key not null,
+    name varchar(255) default null,
+    email varchar (255) default null,
+    mobile_number varchar(255) default null
 );
